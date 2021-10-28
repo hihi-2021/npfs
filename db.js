8c40246565f0514534781d4fe0dd9eac486c4692
dylan-curtis-reeve <d.curtis.reeve@gmail.com>
2021-10-29 10:05:49 +1300

    Update table references in db.js

--- db.js
+++ db.js
@@ -2,15 +2,15 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
-module.exports = {
-  getUser: getUser,
-  getUsers: getUsers
-}
-
 function getUsers (db = connection) {
-  return db('users').select()
+  return db('trainers').select()
 }
 
 function getUser (id, db = connection) {
-  return db('users').where('id', id).first()
+  return db('trainers').where('id', id).first()
+}
+
+module.exports = {
+  getUser: getUser,
+  getUsers: getUsers
 }
